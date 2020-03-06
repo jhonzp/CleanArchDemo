380642f66f5cd99a21a477659d47989e9599d22d
Jhon Zuñiga <zuni230669>
2020-03-05 22:34:58 -0500

    Commit Service And Repository Interface

---
+++ CleanArch/cleanArch.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,13 @@
+﻿using cleanArch.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cleanArch.Application.Interfaces
+{
+    public interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses();
+    }
+
+}
